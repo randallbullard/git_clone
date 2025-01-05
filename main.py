0df109edcfdad3f78437b371458eb9dc7a61484f
Randall Bullard <44116621+randallbullard@users.noreply.github.com>
2025-01-05 15:41:45 -0500

    Currently prints only public repos

--- main.py
+++ main.py
@@ -15,26 +15,30 @@ load_dotenv()
 gh_key = os.getenv('API_KEY')
 gh_url = os.getenv('BASEURL')
 
-headers = {'Authorization': f'Bearer {gh_key}'}
-# print(headers)
+headers = {
+    "Authorization": f"Bearer {gh_key}",
+    "Accept": "application/vnd.github.v3+json"
+    }
+
+params = {
+       "visibility": "all",
+       "affiliation": "owner",
+       "sort": "full_name"
+    }
 
 owner = 'randallbullard' #str(input('Enter target username: '))
 
 def main():
-    print(f'{gh_url}/users/{owner}/repos')
-    r = httpx.get(f'{gh_url}/users/{owner}/repos', headers=headers) # Build list of repos request
+    print(f'{gh_url}/{owner}/repos')
+    r = httpx.get(f'{gh_url}/users/{owner}/repos', headers = headers, params = params) # Build list of repos request
+    
     if r.status_code == 200:
         r_dict = r.json() # Convert json into dictionary
-        # print(type(r_dict))
-        # print(json.dumps(r_dict, indent=2, sort_keys=True)) # Pretty Printing JSON string back
         for i in range(len(r_dict)):
             for key,value in r_dict[i].items():
                 if key == 'name':
                     print(value)
         
-        
-        # for key in r_dict:
-        #     print(value)
     else:
         print(r.status_code)
     
