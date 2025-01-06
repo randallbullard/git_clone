c29875b338e12dc752ee4dcc895bb534cbb7c6bd
Randall Bullard <44116621+randallbullard@users.noreply.github.com>
2025-01-05 20:00:06 -0500

    reduced complexity, working script

--- clone.py
+++ clone.py
@@ -1,17 +1,8 @@
 # Import Modules
 import os
-import truststore
-truststore.inject_into_ssl()
-from dotenv import load_dotenv
 import httpx
 
-# load the dotenv file
-load_dotenv()
-
-# Environmental Variables
-gh_key = os.getenv('API_KEY')
-
-
+# Read Repo list and clone repos
 with open('/home/randallbullard/programming/python/github_repos/git_clone_script/repos.txt', "r") as repo_list:
     for line in repo_list:
         for repo in line.split():
